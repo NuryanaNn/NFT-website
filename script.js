37edbcc0a871f20b618d034f970026a2e53f7ad6
NuryanaNn <nuryan504@gmail.comescgit clear>
2022-02-25 22:51:47 +0700

    Initial Commit

--- script.js
+++ script.js
@@ -1,3 +1,51 @@
+
+var countDownDate = new Date("Mar 5, 2022 15:37:25").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the element with id="demo"
+  document.getElementById("timesSell").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+
+  document.getElementById("timesSell2").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+
+	document.getElementById("timesSell3").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+
+  document.getElementById("timesSell4").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+
+  document.getElementById("timesSell5").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+
+  document.getElementById("timesSell6").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+
+
+
+  // If the count down is finished, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("timesSell").innerHTML = "EXPIRED";
+  }
+}, 1000);
+
+
+
 let popUpSearch = document.querySelector('.popUpSearch');
 let searchMenu = document.querySelector('#searchMenu');
 
@@ -89,10 +137,6 @@ droped4.onclick = () =>{
 }
 
 
-
-
-
-
 // Hidden Navbar Function
 let burger = document.querySelector('.burger');
 let hiddenNav = document.querySelector('.hiddenNav');
@@ -106,10 +150,38 @@ burger.addEventListener('click', function(){
 });
 
 
-
 // Hidden Nav hiddenNav
 let times = document.querySelector('.fa-times');
 
 times.addEventListener('click', function(){
 	hiddenNav.classList.toggle('active');
-})
+});
+
+
+var swiper = new Swiper(".contentBox", {
+    loop: true,
+    centeredSlides: true,
+    autoplay: {
+        delay: 9500,
+        disabledonINteraction: false,
+    },
+    navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+    breakpoints: {
+        480: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        1024: {
+            slidesPerView: 3,
+        },
+    },
+});
+
+
+
+
